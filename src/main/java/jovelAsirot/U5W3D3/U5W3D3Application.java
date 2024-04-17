0b152590d63541faec5d8172945dbb2d2647b819
Jovelasirot <jovelasirot@gmail.com>
2024-04-17 17:09:31 +0200

    main of es1 moved to correct file

--- src/main/java/jovelAsirot/U5W3D3/U5W3D3Application.java
+++ src/main/java/jovelAsirot/U5W3D3/U5W3D3Application.java
@@ -1,29 +1,13 @@
 package jovelAsirot.U5W3D3;
 
-import jovelAsirot.U5W3D3.es1.entities.Info;
-import jovelAsirot.U5W3D3.es1.entities.InfoAdapter;
-import jovelAsirot.U5W3D3.es1.entities.UserData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class U5W3D3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(U5W3D3Application.class, args);
-
-        Info info = new Info("Joji", "Sanctuary", LocalDate.of(1992, 9, 18));
-
-        InfoAdapter adapter = new InfoAdapter(info);
-
-        UserData userData = new UserData(adapter);
-
-        System.out.println("-------------------- User Data --------------------");
-        System.out.println("Full name:" + userData.getDataSource().getFullName());
-        System.out.println("Age: " + userData.getDataSource().getAge());
-
     }
 
 }
--- src/main/java/jovelAsirot/U5W3D3/es1/MainEs1.java
+++ src/main/java/jovelAsirot/U5W3D3/es1/MainEs1.java
@@ -1,7 +1,21 @@
 package jovelAsirot.U5W3D3.es1;
 
+import jovelAsirot.U5W3D3.es1.entities.DataSource;
+import jovelAsirot.U5W3D3.es1.entities.Info;
+import jovelAsirot.U5W3D3.es1.entities.InfoAdapter;
+
+import java.time.LocalDate;
+
 public class MainEs1 {
     public static void main(String[] args) {
+
+        Info info = new Info("Joji", "Sanctuary", LocalDate.of(1992, 9, 18));
+
+        DataSource adaptor = new InfoAdapter(info);
+
+        System.out.println("------------- User data -------------");
+        System.out.println("Full name: " + adaptor.getFullName());
+        System.out.println("Ege: " + adaptor.getAge());
 
     }
 }
